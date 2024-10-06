4df1c5c9d42ca3a0930d12afcad445a37c194b00
Mirakhmad333 <akbarovmirahmad7777@gmail.com>
2024-10-06 22:09:54 +0500

    add all files

---
+++ script.js
@@ -0,0 +1,17 @@
+let userName = prompt ('Введите свое имя');
+console.log('Меня зовут ' + userName);
+let userAge = prompt('Bведите свой возраст');
+console.log('Мой возраст ' + userAge);
+let primer = prompt ('5 + 3 = ?');
+console.log('Пример 1: 5 + 3 = 8 Ваш ответ: ' + primer);
+let primer2 = prompt ('5 - 3 = ?');
+console.log('Пример 2: 5 - 3 = 2 Ваш ответ: ' + primer2);
+let primer3 = prompt ('5 * 3 = ?');
+console.log('Пример 3: 5 * 3 = 15 Ваш ответ: ' + primer3);
+let primer4 = prompt ('5 / 1 = ?');
+console.log('Пример 4: 5 / 1 = 5 Ваш ответ: ' + primer4);
+let primer5 = prompt ('10 % 3 = ?');
+console.log('Пример 5: 10 % 3 = 1 Ваш ответ: ' + primer5);
+
+
+
---
+++ secondtask.js
@@ -0,0 +1,19 @@
+// Задание 2
+
+let X, Y, Z, res;
+
+
+X = parseFloat(prompt("Bведите первое число: "));
+console.log('Первое число ' + X);
+Y = parseFloat(prompt("Bведите второе число: "));
+console.log('Второе число '+ Y);
+Z = parseFloat(prompt("Bведите третье число: "));
+console.log('Третье число '+ Z);
+
+
+res = (X + Y + Z) / 3;
+alert('Ваше среднее число ' + res);
+console.log('Ваше среднее число ' + res);
+
+
+
